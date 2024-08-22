65b8fc8383637f13c89edf2cdc2266bad8b3c255
Daniel Zhu <daniel@inxeption.com>
2024-08-21 23:20:17 -0700

    Add .storybook directory back.

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from "@storybook/react";
+import "../stories/scss/global.scss";
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;
