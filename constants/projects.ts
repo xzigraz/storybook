86e6b1ba459068f87be0b32c792aff582f7053c6
xzigraz <danielzzhu@gmail.com>
2024-12-23 23:15:57 -0800

    Add projects object.

---
+++ constants/projects.ts
@@ -0,0 +1,12 @@
+export default interface PROJECT {
+	name: string
+	label: string
+	slug: string | null | undefined
+}
+export const Projects: PROJECT[] = [
+	{
+		name: "thedepartedonline",
+		label: "The Departed Online",
+		slug: "the-departed-online"
+	}
+]
