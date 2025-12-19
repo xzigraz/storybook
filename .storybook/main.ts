d359aec03fc7592e9e899b380a9d194e4b47582c
xzigraz <danielzzhu@gmail.com>
2025-12-19 00:05:38 -0800

    Fix storybook issue.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,8 +10,7 @@ const config: StorybookConfig = {
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@chromatic-com/storybook",
-		"@storybook/addon-interactions",
-		"@storybook/addon-styling-webpack"
+		"@storybook/addon-interactions"
 	],
 	framework: {
 		name: "@storybook/nextjs",
